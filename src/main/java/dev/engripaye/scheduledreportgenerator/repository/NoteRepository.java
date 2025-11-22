99aa1c4cccebc3b77f8702846c32989d82fd643a
Engr. Ipaye Babatunde <b.tunde.ipaye@gmail.com>
2025-11-22 05:21:52 +0100

    Update NoteRepository.java
    Co-Authored-By: Progitdev <244293117+Progitdev@users.noreply.github.com>

--- src/main/java/dev/engripaye/scheduledreportgenerator/repository/NoteRepository.java
+++ src/main/java/dev/engripaye/scheduledreportgenerator/repository/NoteRepository.java
@@ -4,4 +4,6 @@ import dev.engripaye.scheduledreportgenerator.entity.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NoteRepository extends JpaRepository<Note, Long> {
+
+    //'/ Additional query methods can be defined here if needed'
 }
